ecb72e7c77cea4a3faca07376f9eb8262997baf9
domnichev77 <domnichev77@gmail.com>
2026-06-18 15:27:11 +0500

    Update hh_parser.py

--- hh_parser.py
+++ hh_parser.py
@@ -1,28 +1,22 @@
 import requests
 import os
-import re
 
 TOKEN = os.environ["TELEGRAM_TOKEN"]
 CHAT_ID = os.environ["TELEGRAM_CHAT_ID"]
 
-url = "https://ust-kamenogorsk.hh.kz/search/vacancy?area=194"
-
 html = requests.get(
-    url,
+    "https://ust-kamenogorsk.hh.kz/search/vacancy?area=194",
     headers={"User-Agent": "Mozilla/5.0"}
 ).text
 
-matches = re.findall(r'/vacancy/(\d+)', html)
+idx = html.find("134222077")
 
-msg = (
-    f"found={len(matches)}\n\n"
-    + "\n".join(matches[:30])
-)
+fragment = html[max(0, idx-1000):idx+2000]
 
 requests.post(
     f"https://api.telegram.org/bot{TOKEN}/sendMessage",
     data={
         "chat_id": CHAT_ID,
-        "text": msg[:4000]
+        "text": fragment[:4000]
     }
 )
