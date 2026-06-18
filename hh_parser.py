3796b7478bf91a31880c5d2d0e00445f88dd5b17
domnichev77 <domnichev77@gmail.com>
2026-06-18 07:36:28 +0500

    Update hh_parser.py

--- hh_parser.py
+++ hh_parser.py
@@ -5,16 +5,18 @@ TOKEN = os.environ["TELEGRAM_TOKEN"]
 CHAT_ID = os.environ["TELEGRAM_CHAT_ID"]
 
 r = requests.get(
-    "https://api.hh.ru/suggests/areas",
+    "https://api.hh.ru/vacancies",
     params={
-        "text": "Усть-Каменогорск"
+        "area": 194,
+        "per_page": 10,
+        "page": 0
     },
     headers={
         "User-Agent": "JobHunterBot/1.0"
     }
 )
 
-msg = r.text[:3500]
+msg = f"status={r.status_code}\n\n{r.text[:3000]}"
 
 requests.post(
     f"https://api.telegram.org/bot{TOKEN}/sendMessage",
