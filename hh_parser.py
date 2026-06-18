f196d8f2c548cdfcb42dbf431d2aa441bb766c0f
domnichev77 <domnichev77@gmail.com>
2026-06-18 15:24:21 +0500

    Update hh_parser.py

--- hh_parser.py
+++ hh_parser.py
@@ -1,23 +1,22 @@
 import requests
 import os
+import re
 
 TOKEN = os.environ["TELEGRAM_TOKEN"]
 CHAT_ID = os.environ["TELEGRAM_CHAT_ID"]
 
 url = "https://ust-kamenogorsk.hh.kz/search/vacancy?area=194"
 
-r = requests.get(
+html = requests.get(
     url,
-    headers={
-        "User-Agent": "Mozilla/5.0"
-    },
-    timeout=20
-)
+    headers={"User-Agent": "Mozilla/5.0"}
+).text
+
+matches = re.findall(r'/vacancy/(\d+)', html)
 
 msg = (
-    f"status={r.status_code}\n"
-    f"len={len(r.text)}\n\n"
-    f"{r.text[:1000]}"
+    f"found={len(matches)}\n\n"
+    + "\n".join(matches[:30])
 )
 
 requests.post(
